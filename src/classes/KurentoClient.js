25a295e6a643cf2d246a40b67c36b0ca92a28ae1
Agam Theos <agamtheos@gmail.com>
2023-08-05 16:41:04 +0700

    build: remove kurento

--- src/classes/KurentoClient.js
+++ src/classes/KurentoClient.js
@@ -1,34 +1,34 @@
-const kurento = require('kurento-client');
-const minimist = require('minimist');
+// const kurento = require('kurento-client');
+// const minimist = require('minimist');
 
-const env = require('../config/env');
+// const env = require('../config/env');
 
-let kurentoClient = null
+// let kurentoClient = null
 
-module.exports = function KurentoClient() {
-    this.getKurentoClient = (callback) => {
-        const argv = minimist(process.argv.slice(2), {
-            default: {
-                as_uri: env.as_uri,
-                ws_uri: env.ws_uri
-            }
-        })
+// module.exports = function KurentoClient() {
+//     this.getKurentoClient = (callback) => {
+//         const argv = minimist(process.argv.slice(2), {
+//             default: {
+//                 as_uri: env.as_uri,
+//                 ws_uri: env.ws_uri
+//             }
+//         })
 
-        if (kurentoClient !== null) {
-            return callback(null, kurentoClient);
-        }
+//         if (kurentoClient !== null) {
+//             return callback(null, kurentoClient);
+//         }
     
-        kurento(argv.ws_uri, function(error, _kurentoClient) {
-            console.log('KurentoClient connected at address ' + argv.ws_uri)
-            if (error) {
-                let message = 'Coult not find media server at address ' + argv.ws_uri;
-                return callback(message + ". Exiting with error " + error);
-            }
+//         kurento(argv.ws_uri, function(error, _kurentoClient) {
+//             console.log('KurentoClient connected at address ' + argv.ws_uri)
+//             if (error) {
+//                 let message = 'Coult not find media server at address ' + argv.ws_uri;
+//                 return callback(message + ". Exiting with error " + error);
+//             }
     
-            kurentoClient = _kurentoClient;
-            callback(null, kurentoClient);
-        });
-    }
+//             kurentoClient = _kurentoClient;
+//             callback(null, kurentoClient);
+//         });
+//     }
 
-    this.get = () => kurentoClient
-}
+//     this.get = () => kurentoClient
+// }
